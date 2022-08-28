9b1de096dba82b54502fa4247e869fdbc62fe0a1
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-08-28 17:49:45 +0400

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 import store from './redux/state.js';
 
@@ -9,22 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let renderEntireTree = (state) => {
   root.render(
-    <React.StrictMode>
       <App state={state} dispatch={store.dispatch.bind(store)} />
-    </React.StrictMode>
   );
 };
 
 renderEntireTree(store.getState());
-
-reportWebVitals();
-
 store.subscribe(renderEntireTree);
-// export let renderEntireTree = ()=> {
-//   const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App state = {state} addPost={addPost}/>
-//   </React.StrictMode>
-// );
-// }
