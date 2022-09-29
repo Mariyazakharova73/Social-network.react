90218b711debf77669bc3ef6dc20e30c5e470111
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-09-29 18:58:05 +0400

    readme

--- src/App.js
+++ src/App.js
@@ -19,11 +19,7 @@ const App = (props) => {
         <div className="app-wrapper-content">
           <Routes>
             <Route path="/profile" element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
-
             <Route path="/dialogs/*" element={<Dialogs store={props.store} />} />
-
-
-            
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} />
